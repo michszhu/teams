d704ba55cc8c2542b82a0f39f4d22b773b08a653
michzzzm <michzzzm@gmail.com>
2016-12-22 15:29:59 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,82 +1,202 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
-define('APPLICATION_NAME', 'Google Sheets API PHP Quickstart');
-define('CREDENTIALS_PATH', '~/.credentials/sheets.googleapis.com-php-quickstart.json');
-define('CLIENT_SECRET_PATH', __DIR__ . '/client_secret.json');
-// If modifying these scopes, delete your previously saved credentials
-// at ~/.credentials/sheets.googleapis.com-php-quickstart.json
-define('SCOPES', implode(' ', array(
-  Google_Service_Sheets::SPREADSHEETS_READONLY)
-));
-if (php_sapi_name() != 'cli') {
-  throw new Exception('This application must be run on the command line.');
+// apitest.php
+// by Karl Kranich - karl.kranich.org
+// version 3.1 - edited query section
+require_once realpath(dirname(__FILE__) . '/vendor/autoload.php');
+include_once "google-api-php-client/examples/templates/base.php";
+$client = new Google_Client();
+/************************************************
+  ATTENTION: Fill in these values, or make sure you
+  have set the GOOGLE_APPLICATION_CREDENTIALS
+  environment variable. You can get these credentials
+  by creating a new Service Account in the
+  API console. Be sure to store the key file
+  somewhere you can get to it - though in real
+  operations you'd want to make sure it wasn't
+  accessible from the webserver!
+ ************************************************/
+putenv("GOOGLE_APPLICATION_CREDENTIALS=service-account-credentials.json");
+if ($credentials_file = checkServiceAccountCredentialsFile()) {
+  // set the location manually
+  $client->setAuthConfig($credentials_file);
+} elseif (getenv('GOOGLE_APPLICATION_CREDENTIALS')) {
+  // use the application default credentials
+  $client->useApplicationDefaultCredentials();
+} else {
+  echo missingServiceAccountDetailsWarning();
+  exit;
 }
-/**
- * Returns an authorized API client.
- * @return Google_Client the authorized client object
- */
-function getClient() {
-  $client = new Google_Client();
-  $client->setApplicationName(APPLICATION_NAME);
-  $client->setScopes(SCOPES);
-  $client->setAuthConfig(CLIENT_SECRET_PATH);
-  $client->setAccessType('offline');
-  // Load previously authorized credentials from a file.
-  $credentialsPath = expandHomeDirectory(CREDENTIALS_PATH);
-  if (file_exists($credentialsPath)) {
-    $accessToken = json_decode(file_get_contents($credentialsPath), true);
-  } else {
-    // Request authorization from the user.
-    $authUrl = $client->createAuthUrl();
-    printf("Open the following link in your browser:\n%s\n", $authUrl);
-    print 'Enter verification code: ';
-    $authCode = trim(fgets(STDIN));
-    // Exchange authorization code for an access token.
-    $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
-    // Store the credentials to disk.
-    if(!file_exists(dirname($credentialsPath))) {
-      mkdir(dirname($credentialsPath), 0700, true);
-    }
-    file_put_contents($credentialsPath, json_encode($accessToken));
-    printf("Credentials saved to %s\n", $credentialsPath);
-  }
-  $client->setAccessToken($accessToken);
-  // Refresh the token if it's expired.
-  if ($client->isAccessTokenExpired()) {
-    $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
-    file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
-  }
-  return $client;
-}
-/**
- * Expands the home directory alias '~' to the full path.
- * @param string $path the path to expand.
- * @return string the expanded path.
- */
-function expandHomeDirectory($path) {
-  $homeDirectory = getenv('HOME');
-  if (empty($homeDirectory)) {
-    $homeDirectory = getenv('HOMEDRIVE') . getenv('HOMEPATH');
-  }
-  return str_replace('~', realpath($homeDirectory), $path);
-}
-// Get the API client and construct the service object.
-$client = getClient();
+$client->setApplicationName("Sheets API Testing");
+$client->setScopes(['https://www.googleapis.com/auth/drive','https://spreadsheets.google.com/feeds']);
+// Some people have reported needing to use the following setAuthConfig command
+// which requires the email address of your service account (you can get that from the json file)
+// $client->setAuthConfig(["type" => "service_account", "client_email" => "my-service-account@developer.gserviceaccount.com"]);
+// The file ID was copied from a URL while editing the sheet in Chrome
+$fileId = '15byt2tfdaHmaEpdwd4UYGWs70Eaej8edkQ2dS8x4mIk';
+// Access Token is used for Steps 2 and beyond
+$tokenArray = $client->fetchAccessTokenWithAssertion();
+$accessToken = $tokenArray["access_token"];
+// Section 1: Uncomment to get file metadata with the drive service
+// This is also the service that would be used to create a new spreadsheet file
+$service = new Google_Service_Drive($client);
+$results = $service->files->get($fileId);
+// var_dump($results);
+// Section 2: Uncomment to get list of worksheets
+// $url = "https://spreadsheets.google.com/feeds/worksheets/$fileId/private/full";
+// $method = 'GET';
+// $headers = ["Authorization" => "Bearer $accessToken"];
+// $httpClient = new GuzzleHttp\Client(['headers' => $headers]);
+// $resp = $httpClient->request($method, $url);
+// $body = $resp->getBody()->getContents();
+// $code = $resp->getStatusCode();
+// $reason = $resp->getReasonPhrase();
+// echo "$code : $reason\n\n";
+// echo "$body\n";
+// Section 3: Uncomment to get the table data
+/* $url = "https://spreadsheets.google.com/feeds/list/$fileId/od6/private/full";
+ $method = 'GET';
+ $headers = ["Authorization" => "Bearer $accessToken", "GData-Version" => "3.0"];
+ $httpClient = new GuzzleHttp\Client(['headers' => $headers]);
+ $resp = $httpClient->request($method, $url);
+ $body = $resp->getBody()->getContents();
+ $code = $resp->getStatusCode();
+ $reason = $resp->getReasonPhrase();
+ echo "$code : $reason\n\n";
+ echo "$body\n";  */
+// Section 4: Uncomment to add a row to the sheet
+// $url = "https://spreadsheets.google.com/feeds/list/$fileId/od6/private/full";
+// $method = 'POST';
+// $headers = ["Authorization" => "Bearer $accessToken", 'Content-Type' => 'application/atom+xml'];
+// $postBody = '<entry xmlns="http://www.w3.org/2005/Atom" xmlns:gsx="http://schemas.google.com/spreadsheets/2006/extended"><gsx:gear>more gear</gsx:gear><gsx:quantity>44</gsx:quantity></entry>';
+// $httpClient = new GuzzleHttp\Client(['headers' => $headers]);
+// $resp = $httpClient->request($method, $url, ['body' => $postBody]);
+// $body = $resp->getBody()->getContents();
+// $code = $resp->getStatusCode();
+// $reason = $resp->getReasonPhrase();
+// echo "$code : $reason\n\n";
+// echo "$body\n";
+// Section 5: Uncomment to edit a row
+// You'll need to get the etag and row ID, and send a PUT request to the edit URL
+// $rowid = 'cre1l';                 // got this and the etag from the table data output from section 3
+// $etag = 'NQ8SVE8fDSt7ImA.';
+// $url = "https://spreadsheets.google.com/feeds/list/$fileId/od6/private/full/$rowid";
+// $method = 'PUT';
+// $headers = ["Authorization" => "Bearer $accessToken", 'Content-Type' => 'application/atom+xml', 'GData-Version' => '3.0'];
+// $postBody = "<entry xmlns=\"http://www.w3.org/2005/Atom\" xmlns:gsx=\"http://schemas.google.com/spreadsheets/2006/extended\" xmlns:gd=\"http://schemas.google.com/g/2005\" gd:etag='&quot;$etag&quot;'><id>https://spreadsheets.google.com/feeds/list/$fileid/od6/$rowid</id><gsx:gear>phones</gsx:gear><gsx:quantity>6</gsx:quantity></entry>";
+// $httpClient = new GuzzleHttp\Client(['headers' => $headers]);
+// $resp = $httpClient->request($method, $url, ['body' => $postBody]);
+// $body = $resp->getBody()->getContents();
+// $code = $resp->getStatusCode();
+// $reason = $resp->getReasonPhrase();
+// echo "$code : $reason\n\n";
+// echo "$body\n";
+// Section 6: Uncomment to parse table data with SimpleXML  
+ /*$url = "https://spreadsheets.google.com/feeds/list/$fileId/od6/private/full";
+ $method = 'GET';
+ $headers = ["Authorization" => "Bearer $accessToken", "GData-Version" => "3.0"];
+ $httpClient = new GuzzleHttp\Client(['headers' => $headers]);
+ $resp = $httpClient->request($method, $url);
+ $body = $resp->getBody()->getContents();
+ $tableXML = simplexml_load_string($body);
+ 
+ echo xmlObjToArr ($tableXML)
+ 
+function xmlObjToArr($obj) { 
+        $namespace = $obj->getDocNamespaces(true); 
+        $namespace[NULL] = NULL; 
+        
+        $children = array(); 
+        $attributes = array(); 
+        $name = strtolower((string)$obj->getName()); 
+        
+        $text = trim((string)$obj); 
+        if( strlen($text) <= 0 ) { 
+            $text = NULL; 
+        } 
+        
+        // get info for all namespaces 
+        if(is_object($obj)) { 
+            foreach( $namespace as $ns=>$nsUrl ) { 
+                // atributes 
+                $objAttributes = $obj->attributes($ns, true); 
+                foreach( $objAttributes as $attributeName => $attributeValue ) { 
+                    $attribName = strtolower(trim((string)$attributeName)); 
+                    $attribVal = trim((string)$attributeValue); 
+                    if (!empty($ns)) { 
+                        $attribName = $ns . ':' . $attribName; 
+                    } 
+                    $attributes[$attribName] = $attribVal; 
+                } 
+                
+                // children 
+                $objChildren = $obj->children($ns, true); 
+                foreach( $objChildren as $childName=>$child ) { 
+                    $childName = strtolower((string)$childName); 
+                    if( !empty($ns) ) { 
+                        $childName = $ns.':'.$childName; 
+                    } 
+                    $children[$childName][] = xmlObjToArr($child); 
+                } 
+            } 
+        } 
+        
+        return array( 
+            'name'=>$name, 
+            'text'=>$text, 
+            'attributes'=>$attributes, 
+            'children'=>$children 
+        ); 
+    } 
+	
+	*/
+	
+ /*echo "Rows:\n";
+ foreach ($tableXML->entry as $entry) {
+   $etag = $entry->attributes('gd', TRUE);
+   $id = $entry->id;
+   echo "etag: $etag\n";
+   echo "id: $id\n";
+   foreach ($entry->children('gsx', TRUE) as $column) {
+     $colName = $column->getName();
+     $colValue = $column;
+     echo "$colName : $colValue\n";
+   }
+ } */
+// Section 7: Uncomment to query for a subset of rows and parse data with SimpleXML
+// $url = "https://spreadsheets.google.com/feeds/list/$fileId/od6/private/full?sq=quantity>9";
+// $myQuery = 'quantity>9';  // and here is an example with a space in it: $myQuery = 'gear="mifi device"';
+// $method = 'GET';
+// $headers = ["Authorization" => "Bearer $accessToken", "GData-Version" => "3.0"];
+// $httpClient = new GuzzleHttp\Client(['headers' => $headers]);
+// $resp = $httpClient->request($method, $url, ['query' => ['sq' => $myQuery]]);
+// $body = $resp->getBody()->getContents();
+// $tableXML = simplexml_load_string($body);
+// echo "Rows:\n";
+// foreach ($tableXML->entry as $entry) {
+//   $etag = $entry->attributes('gd', TRUE);
+//   $id = $entry->id;
+//   echo "etag: $etag\n";
+//   echo "id: $id\n";
+//   foreach ($entry->children('gsx', TRUE) as $column) {
+//     $colName = $column->getName();
+//     $colValue = $column;
+//     echo "$colName : $colValue\n";
+//   }
+// }
+
+
+
 $service = new Google_Service_Sheets($client);
-
-
 $spreadsheetId = '1LhCT9KRfMrXinRyphcBn1jz3JIUh5LQSli9mQFmOc7w';
 $range = 'signups!A1:D';
 $response = $service->spreadsheets_values->get($spreadsheetId, $range);
 $values = $response->getValues();
-
 // find index of columns of info (name, grade, events)
 $Cfn = array_search ( 'First Name', $values[0]);
 $Cln = array_search ( 'Last Name', $values[0]);
 $Cgrade = array_search ( 'Grade', $values[0]);
 $Cevents = array_search ( 'Events', $values[0]);
 $ppl = array();
-
 // add people and their info
 for ($i = 1 ; $i < count ($values) ; $i++){
 	$info = $values[$i];
@@ -114,10 +234,8 @@ for ($i = 2 ; $i < count ($values); $i++){
 		$events = array ();
 		$okeyevents = array ();
 		$dokeyevents = array ();
-
 		for ($p = $i ; $p<$k; $p++){ // per event
 			$event = $values [$p][1];
-
 			 echo "\n". $event. ' : ';
 			$countevents++;
 			
@@ -150,10 +268,7 @@ for ($i = 2 ; $i < count ($values); $i++){
 echo json_encode ($blocks, JSON_PRETTY_PRINT);
 echo json_encode ($okeyblocks, JSON_PRETTY_PRINT);
 echo json_encode ($dokeyblocks, JSON_PRETTY_PRINT);
-
 echo "\n";
 echo 'TOTAL EVENT REQUESTS: ' . $countins. "\n";
 echo 'TOTAL PEOPLE: ' . count ($ppl). "\n";
 echo 'TOTAL EVENTS: ' . $countevents;
-
-
