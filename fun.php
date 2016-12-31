3544160a7fe83fbe138289728f43166bb826d368
michzzzm <michzzzm@gmail.com>
2016-12-31 01:42:27 -0600

    Update fun.php

--- fun.php
+++ fun.php
@@ -373,7 +373,7 @@ function isOnTeam ($person, $team){
 	return FALSE;
 }
 function isUnderEvented ($person){
-	if ($person['numevents'] < 4)
+	if ($person['numevents'] < 5)
 		return TRUE;
 	else{
 		// echo 'OVER EVENTED . ' . $person['numevents'] . $person['name'];
