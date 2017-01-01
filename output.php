ae7039138ef23123e11575c662bc59411f0acc7c
michzzzm <michzzzm@gmail.com>
2017-01-01 12:49:06 -0600

    Update output.php

--- output.php
+++ output.php
@@ -1,5 +1,5 @@
 <?php
-// define('__ROOT__', dirname(dirname(__FILE__))); 
+define('__ROOT__', dirname(dirname(__FILE__))); 
 require_once(__ROOT__.'/teams/input.php'); 
 
 require_once(__ROOT__.'/teams/addcats.php'); 
